004da2e43aac2f0a8dd1d070d2f820b6d9aed213
kkuras <carretta2315@gmail.com>
2024-12-18 21:37:08 -0300

    raw / alexandrite added

--- src/main/java/net/kkura/tutorialmod/TutorialMod.java
+++ src/main/java/net/kkura/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.kkura.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.kkura.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,7 +31,7 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,6 +45,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
