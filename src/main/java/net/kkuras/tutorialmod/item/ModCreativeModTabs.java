63b63e48fc22aa270095c5542268df177ea63d00
kkuras <carretta2315@gmail.com>
2024-12-22 19:13:22 -0300

    custom recipes and loot tables

--- src/main/java/net/kkuras/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/kkuras/tutorialmod/item/ModCreativeModTabs.java
@@ -33,6 +33,8 @@ public class ModCreativeModTabs {
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModBlocks.ALEXANDRITE_BLOCK.get());
                         output.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK.get());
+                        output.accept(ModBlocks.ALEXANDRITE_ORE.get());
+                        output.accept(ModBlocks.ALEXANDRITE_DEEPSLATE_ORE.get());
 
 
                     }).build());
